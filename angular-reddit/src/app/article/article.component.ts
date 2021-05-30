5934210d6c7e2d8d39c83341f605311d877ac644
houyingshi@163.com <houyingshi@163.com>
2021-05-30 14:46:49 +0800

    init new component

--- angular-reddit/src/app/article/article.component.ts
+++ angular-reddit/src/app/article/article.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
@@ -9,23 +10,33 @@ import { Component, OnInit } from '@angular/core';
   }
 })
 export class ArticleComponent implements OnInit {
-  votes: number;
-  title: string;
-  link: string;
+  // votes: number;
+  // title: string;
+  // link: string;
+  // votes: number;
+  // title: string;
+  // link: string;
+  @Input()
+  article!: Article;
 
   constructor() { 
-    this.title = "Angular 12";
-    this.link = "http://angular.io";
-    this.votes = 10;
+    // this.title = "Angular 12";
+    // this.link = "http://angular.io";
+    // this.votes = 10;
+    // this.article = new Article("Angular 12", "http://angular.io", 10);
   }
 
   voteUp(): boolean{
-    this.votes += 1;
+    // this.votes += 1;
+    // this.article.votes += 1;
+    this.article.voteUp();
     return false;
   }
 
   voteDown(): boolean{
-    this.votes -= 1;
+    // this.votes -= 1;
+    // this.article.votes -= 1;
+    this.article.voteDown();
     return false;
   }
 
